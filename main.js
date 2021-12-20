0ba9770e9eb4b91585cd3b5606d2fb272d804d3b
wfrsnk <57827569+wfrsnk@users.noreply.github.com>
2021-12-20 21:55:12 +0300

    2

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+fetch('http://www.cbr.ru/scripts/XML_daily.asp', {
+    mode: "no-cors",
+    method: "GET",
+})
+    .then(function(res) {
+        return res.text();
+    }).then(function(data) {
+        console.log(data);
+    })
